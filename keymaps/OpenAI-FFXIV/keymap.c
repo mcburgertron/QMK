b8fb01ca9563324bf10c07231f15fe0b40352fd9
mcburgertron <verbprobe@outlook.com>
2025-02-07 23:46:26 -0800

    Changed layer 4 LCTL to LSFT.

--- keymaps/OpenAI-FFXIV/keymap.c
+++ keymaps/OpenAI-FFXIV/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______,
 
         // Row 2
-        KC_LCTL, KC_LALT, KC_TAB, LCTL(KC_SPC),
+        KC_LSFT, KC_LALT, KC_TAB, LCTL(KC_SPC),
 
         // Row 3
         _______, _______, _______, _______)};
